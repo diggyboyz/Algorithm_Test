d8efc4b11399688b6f4a7fe8bac7b7ef6c33cf2f
Jaehoon Jang <jangjh971008@naver.com>
2021-11-06 01:49:35 +0900

    1-2 치킨배달 코드 추가

--- 1주차/2-2.py
+++ 1주차/2-2.py
@@ -1,6 +1,11 @@
-n, save = input().split()
-n = int(n)
-save = int(save)
+# n, save = input().split()
+# n = int(n)
+# save = int(save)
+
+import sys
+from itertools import combinations
+
+n, save = map(int, sys.stdin.readline().split())
 
 houseList = []
 chickenList = []
@@ -16,22 +21,18 @@ for i in range(n):
         elif arr[i][j] == 2:
             chickenList.append([i, j])
 
+options = list(combinations(chickenList, save))
 selected = []
-for chicken in chickenList:
-    distanceList = []
-    for house in houseList:
-        distance = abs(chicken[0] - house[0]) + abs(chicken[1] - house[1])
-        distanceList.append(distance)
 
-    selected.append([sum(distanceList), distanceList])
+for option in options:
+    distance = 0
+    for hx, hy in houseList:
+        distanceList = []
+        for cx, cy in option:
+            temp = abs(hx - cx) + abs(hy - cy)
+            distanceList.append(temp)
+            
+        distance += min(distanceList)
+    selected.append(distance)
 
-selected = sorted(selected)[0:save]
-
-compare = selected[0][1]
-
-for S in selected:
-    for idx, (val, com) in enumerate(zip(S[1], compare)):
-        if val < com:
-            compare[idx] = val
-
-print(sum(compare))
+print(min(selected))
