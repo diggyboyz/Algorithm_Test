b98f4ef904081d011470ea77e2445efe96d4dde0
Jaehoon Jang <jangjh971008@naver.com>
2021-11-07 00:40:43 +0900

    폴더 생성

--- 1주차/재훈/1-1.py
+++ 1주차/재훈/1-1.py
@@ -1,15 +1,19 @@
+# 뱀의 방향
 directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 snake = []
-commands = []
 
+# 보드의 크기
 n = int(input())
+# 보드 초기화
 board = [[0 for col in range(n)] for row in range(n)]
 
+# 사과
 k = int(input())
 for idx in range(k):
     i, j = input().split()
     board[int(i) - 1][int(j) - 1] = 1
 
+# 명령어
 L = int(input())
 dict = {}
 for i in range(L):
@@ -17,9 +21,10 @@ for i in range(L):
     dict[int(X)] = C
 
 i, j, d = 0, -1, 0
-lifeCount = 0, 0
+lifeCount = 0
 
 while True:
+    lifeCount += 1
     if lifeCount in dict.keys():
         if dict[lifeCount] == 'D':
             d += 1
@@ -29,8 +34,6 @@ while True:
     i += directions[d % 4][0]
     j += directions[d % 4][1]
 
-    lifeCount += 1
-
     if i >= n or j >= n or i < 0 or j < 0:
         break
 
@@ -38,9 +41,10 @@ while True:
         break
     else:
         snake.append([i, j])
-
+    
     if board[i][j] != 1 and len(snake) > 1:
         snake.pop(0)
-
+    
+    # 보드에 사과를 먹고 없어져야함 -> 이걸?
 
 print(lifeCount)
--- 1주차/재훈/1-3.py
+++ 1주차/재훈/1-3.py
@@ -14,25 +14,28 @@ arr = [0 for _ in range(n)]
 
 for i in range(n):
     arr[i] = list(map(int, input().split()))
-
     for j in range(n):
         if arr[i][j] == 1:
-            houseList.append([i, j])
+            houseList.append([i, j]) # 집
         elif arr[i][j] == 2:
-            chickenList.append([i, j])
+            chickenList.append([i, j]) # 치킨
 
+# 남을 치킨 집 수
 options = list(combinations(chickenList, save))
 selected = []
 
+
+
+# 완전 탐색 (브루트 포스)
 for option in options:
     distance = 0
     for hx, hy in houseList:
         distanceList = []
         for cx, cy in option:
-            temp = abs(hx - cx) + abs(hy - cy)
-            distanceList.append(temp)
+            temp = abs(hx - cx) + abs(hy - cy) #거리
+            distanceList.append(temp) # [...]
             
-        distance += min(distanceList)
+        distance += min(distanceList) 
     selected.append(distance)
 
 print(min(selected))
