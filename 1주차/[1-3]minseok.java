c2215bac070cd9d4181f813e3bebfdc47c38cd77
Minse5k <pms16011172@gmail.com>
2021-11-06 23:11:12 +0900

    add 1-3 code

--- 1주차/[1-3]minseok.java
+++ 1주차/[1-3]minseok.java
@@ -1,92 +1,85 @@
-package minseok_boj_3190;
-import java.io.BufferedReader;
+package minseok_boj_15686;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-
-public class Week1 {
+public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		int mapSize = Integer.parseInt(br.readLine());
-		int appleCount = Integer.parseInt(br.readLine());
 		
-		int[][] mapArray = null;
-		mapArray = new int[mapSize][mapSize];
-				
-		for(int i = 0; i < appleCount; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken()) - 1;
-			int y = Integer.parseInt(st.nextToken()) - 1;
-			mapArray[x][y] = 2;
-		}
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		ArrayList<Integer[]> chickenArray = new ArrayList<Integer[]>();
+		ArrayList<Integer[]> houseArray = new ArrayList<Integer[]>();
 		
-		int rotationCount = Integer.parseInt(br.readLine());
-		String[][] rotationArray = null;
-		rotationArray = new String[rotationCount][2];
-		
-		for(int i = 0; i < rotationCount; i++) {
-			st = new StringTokenizer(br.readLine());
-			String x = st.nextToken();
-			String y = st.nextToken();
-			rotationArray[i][0] = x;
-			rotationArray[i][1] = y;
-		}
-		int posX = 0;
-		int posY = 0;
-		Queue<int[]> snakeQueue = new LinkedList<>();
-		int[] distX = {1, 0, -1, 0};
-		int[] distY = {0, 1, 0, -1};
-		int distNow = 0;
-		int time = 1;
-		boolean checkFinish = false;
-		mapArray[0][0] = 1;
-		snakeQueue.add(new int[]{posX, posY});
-		int distTime = 0;
-		
-		while(true) {
-			if(distTime < rotationCount &&Integer.parseInt(rotationArray[distTime][0]) == time - 1) {
-				if(rotationArray[distTime][1].equals("D")) {
-					distNow++;
-				} else if(rotationArray[distTime][1].equals("L")) {
-					distNow--;
-					if(distNow == -1) distNow = 3;
-				}
-				distNow %= 4;
-				distTime++;
-			}
-			int nextPosX = posX + distX[distNow];
-			int nextPosY = posY + distY[distNow];
-			int[] lastPos = snakeQueue.peek();
+		for(int i = 0; i < n; i++) {
+			String[] input = br.readLine().split(" ");
 			
-			if(nextPosX >= mapSize || nextPosY >= mapSize || nextPosX < 0 || nextPosY < 0) {
-				checkFinish = true;
-			} else {
-				if(mapArray[nextPosY][nextPosX] == 0) {
-					mapArray[nextPosY][nextPosX] = 1;
-					mapArray[lastPos[1]][lastPos[0]] = 0;
-					snakeQueue.add(new int[] {nextPosX, nextPosY});
-					snakeQueue.remove();
-					posX = nextPosX;
-					posY = nextPosY;
-				} else if(mapArray[nextPosY][nextPosX] == 1) {
-					checkFinish = true;
-				} else if(mapArray[nextPosY][nextPosX] == 2) {
-					mapArray[nextPosY][nextPosX] = 1;
-					snakeQueue.add(new int[] {nextPosX, nextPosY});
-					posX = nextPosX;
-					posY = nextPosY;
+			for(int j = 0; j < n; j++) {
+				int num = Integer.parseInt(input[j]);
+				
+				if(num == 1) {
+					houseArray.add(new Integer[]{i + 1, j + 1});
+				} else if(num == 2) {
+					chickenArray.add(new Integer[]{i + 1, j + 1});
 				}
-			}	
-			if(checkFinish == true) {
-				break;
 			}
-			time++;
 		}
-		System.out.println(time);
+		int chickenSize = chickenArray.size();
+		int arr[] = new int [chickenSize];
+		
+		for(int i = 0; i < chickenSize; i++) {
+			arr[i] =i;
+		}
+		
+		boolean[] visited = new boolean[chickenSize];
+		Arrays.fill(visited, false);
+		
+		combination(arr, visited, 0, chickenSize, m, houseArray, chickenArray);
+		System.out.println(result);
 	}
+	
+	public static int distance (int x1, int y1, int x2, int y2) {
+		return Math.abs(x1-x2) + Math.abs(y1-y2);
+	}
+	static int result = 99999;
+	
+	public static void combination(int[] arr, boolean[] visited, int start, int n, int r, ArrayList<Integer[]> house, ArrayList<Integer[]> chicken) {
+	    if(r == 0) {
+	    	int sum = 0;
+	        for(int i = 0; i < house.size(); i++) {
+	        	int x1 = house.get(i)[0];
+	        	int y1 = house.get(i)[1];
+	        	int dis = 0;
+	        	int min = 9999999;
+	        	
+	        	for(int j = 0; j < chicken.size(); j++) {
+	        		if(visited[j] != true) continue;
+	        		int x2 = chicken.get(j)[0];
+	        		int y2 = chicken.get(j)[1];
+	        	
+	        		dis = distance(x1, y1, x2, y2);
+	        		if(min > dis) min = dis;
+	        	}
+	        	sum += min;
+	        }
+	        if(result > sum) result = sum;
+	        return;
+	    } 
+
+	    for(int i = start; i<n; i++) {
+	        visited[i] = true;
+	        combination(arr, visited, i + 1, n, r - 1, house, chicken);
+	        visited[i] = false;
+	    }
+	}
+	
 }
+/*		
+*/
